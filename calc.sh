a2a388c4aefe1888994e757ae102b33f51a5fbd2
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-03-02 17:22:06 +0100

    Revert to simpler file structure

--- calc.sh
+++ calc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
-mkdir -p output
-inkomst=$(cat input/lon.txt)
+mkdir -p output/privatpension
+INPUT=upstream/output
+inkomst=$(cat ${INPUT}/lon.txt)
 res=$(echo "scale=0; (${inkomst} * 0.05) / 1" | bc -l)
 echo ${res} > output/privatpension.txt
